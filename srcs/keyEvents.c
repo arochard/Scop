2d2bc23297a8dcf22e2062c1d5dbc5b8846df589
Alexandre ROCHARD <arochard@e1r10p15.42.fr>
2016-09-09 17:00:38 +0200

    urgence

--- srcs/keyEvents.c
+++ srcs/keyEvents.c
@@ -26,17 +26,35 @@ static void		translation_down()
 	
 }
 
+static void		scale_up()
+{
+	g_modelMatrix[0] *= 0.3;
+	g_modelMatrix[5] *= 0.3;
+	g_modelMatrix[10] *= 0.3;
+}
+
+static void		scale_down()
+{
+	g_modelMatrix[0] /= 0.3;
+	g_modelMatrix[5] /= 0.3;
+	g_modelMatrix[10] /= 0.3;
+}
+
 void 			key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
 	window = 0;
 	scancode = 0;
 	mods = 0;
-	if (key == GLFW_KEY_RIGHT && action == GLFW_REPEAT)
+	if (key == GLFW_KEY_RIGHT && (action == GLFW_REPEAT || action == GLFW_PRESS))
 		translation_right();
-	if (key == GLFW_KEY_LEFT && action == GLFW_REPEAT)
+	if (key == GLFW_KEY_LEFT && (action == GLFW_REPEAT || action == GLFW_PRESS))
 		translation_left();
-	if (key == GLFW_KEY_UP && action == GLFW_REPEAT)
+	if (key == GLFW_KEY_UP && (action == GLFW_REPEAT || action == GLFW_PRESS))
 		translation_up();
-	if (key == GLFW_KEY_DOWN && action == GLFW_REPEAT)
+	if (key == GLFW_KEY_DOWN && (action == GLFW_REPEAT || action == GLFW_PRESS))
 		translation_down();
+	if (key == GLFW_KEY_PAGE_UP && (action == GLFW_REPEAT || action == GLFW_PRESS))
+		scale_up();
+	if (key == GLFW_KEY_PAGE_DOWN && (action == GLFW_REPEAT || action == GLFW_PRESS))
+		scale_down();
 }
